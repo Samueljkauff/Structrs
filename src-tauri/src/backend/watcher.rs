bfc75f89f0d9405cd8cad74ce13fb00b5b4114a0
samueljkauff <samueljkauff@gmail.com>
2026-02-06 14:01:12 -0500

    small

--- src-tauri/src/backend/watcher.rs
+++ src-tauri/src/backend/watcher.rs
@@ -4,4 +4,17 @@ fn start() {
     let (tx, rx) = channel();
 
     let mut watcher = RecommendedWatcher::new(tx, Config.default()).unwrap();
+
+    watcher.watch(Path::new("~/Downloads")).unwrap();
+
+    for res in rx {
+        match res {
+            Ok(_) => handle_change(),
+            Err(_) => Err("ERROR"),
+        }
+    }
+}
+
+fn handle_change() {
+    
 }
