af59eea5ee1dd85733d0dba8ff4fde001a388574
Sam Kauffman <samueljkauff@gmail.com>
2026-02-18 21:00:14 -0500

    Seperation

--- src-tauri/src/backend/watcher.rs
+++ src-tauri/src/backend/watcher.rs
@@ -1,53 +1,25 @@
-use std::{fs::{self}, path::Path, sync::mpsc::channel, thread, time::Duration};
+use std::{fs::{self}, path::{Path, PathBuf}, sync::mpsc::channel, thread, time::Duration};
 
-use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{Config, EventKind, RecommendedWatcher, RecursiveMode, Watcher, event::CreateKind};
 use tauri::{AppHandle, Manager};
 
 #[tauri::command]
 pub fn start(app: AppHandle) {
-    let downloads = app
-        .path()
-        .download_dir()
-        .expect("Failed to resolve downloads directory");
 
+    let downloads = initialize_download_directory(app);
     println!("Watching: {:?}", downloads);
 
     thread::spawn(move || {
-        let (tx, rx) = channel();
-
-        let mut watcher =
-            RecommendedWatcher::new(tx, Config::default())
-                .expect("Failed to create watcher");
-
-        watcher
-            .watch(&downloads, RecursiveMode::Recursive)
-            .expect("Failed to watch directory");
-
-        println!("Watcher started");
-
-        for res in rx {
-            match res {
-                Ok(event) => {
-                    for path in event.paths {
-                        if is_file_done_downloading(&path) {
-                            println!("Event detected: {:?}, {:?}", event.kind, path);
-                        }
-}
-                }
-                Err(e) => {
-                    println!("Watch error: {:?}", e);
-                }
-            }
-        }
+        run_watcher(&downloads);
     });
 }
 
-fn is_file_done_downloading(path: &Path) -> bool {
+fn is_file_done_downloading(path: &Path, event_kind: EventKind) -> bool {
 
     if !path.exists() {
         return false;
     }
-        if !path.is_file() {
+    if !path.is_file() {
         return false;
     }
 
@@ -76,5 +48,46 @@ fn is_file_done_downloading(path: &Path) -> bool {
         return false;
     }
 
+    if event_kind != EventKind::Create(CreateKind::File) {
+        return false;
+    }
+
     true
+}
+
+fn initialize_download_directory(app: AppHandle) -> PathBuf {
+        app.path()
+        .download_dir()
+        .expect("Failed to resolve downloads directory")
+}
+
+fn run_watcher(downloads: &Path) {
+        let (tx, rx) = channel();
+
+        let mut watcher =
+            RecommendedWatcher::new(tx, Config::default())
+                .expect("Failed to create watcher");
+
+        watcher
+            .watch(&downloads, RecursiveMode::Recursive)
+            .expect("Failed to watch directory");
+
+        println!("Watcher started");
+
+        for res in rx {
+            match res {
+                Ok(event) => {
+                    for path in event.paths {
+                        let event_kind = event.kind;
+                        if is_file_done_downloading(&path, event_kind) {
+                            println!("Event detected: {:?}, {:?}", event.kind, path);
+                        }
+                    }   
+                }
+                Err(e) => {
+                    println!("Watch error: {:?}", e);
+                }
+            }
+        }
+            
 }
